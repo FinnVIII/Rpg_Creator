73af1eb07e90d043f034f93897b5029a39b2d742
AHRGO <victor.030farias@gmail.com>
2022-05-22 19:33:01 -0300

    "Adicionada Lógica Esquisita"

--- manual.h
+++ manual.h
@@ -2,16 +2,21 @@
 #define MANUAL_H_INCLUDED
 
     #define MAX_CARACTERES 100
-    #define NUM_PALAVRAS 6
+    //#define NUM_PALAVRAS 6
 
-    char tipos[NUM_PALAVRAS][MAX_CARACTERES]={{"Bartender"},{"Coureiro"},{"Alquimista"},
-                                              {"Ferreiro"},{"Ourives"},{"Curador"}};
+    int qtd_tipos = 7;
+    int qtd_nomes_m = 5;
+    int qtd_nomes_f = 6;
 
-    char Nomes_M[NUM_PALAVRAS][MAX_CARACTERES]={{"Ammon"},{"Orgrin"},{"Sir Ebonheart"},
+
+    char tipos[qtd_tipos][MAX_CARACTERES]={{"Bartender"},{"Coureiro"},{"Alquimista"},
+                                              {"Ferreiro"},{"Ourives"},{"Curador"}, {"Aventureiro"}, {"Mercenário"}};
+
+    char Nomes_M[qtd_nomes_m][MAX_CARACTERES]={{"Ammon"},{"Orgrin"},{"Sir Ebonheart"},
                                                 {"Zhakir"},{"Balthazar"},{"Dragomir"}};
 
-    char Nomes_F[NUM_PALAVRAS][MAX_CARACTERES]={{"Winnie"},{"Hannah"},{"Aradia"},
-                                                {"Sylvi"},{"Julia"},{"Lidia"}};
+    char Nomes_F[qtd_nomes_f][MAX_CARACTERES]={{"Winnie"},{"Hannah"},{"Aradia"},
+                                                {"Sylvi"},{"Julia"},{"Lidia"}, {"Anna"}};
 
 
 #endif // MANUAL_H_INCLUDED
--- projeto_rpg.c
+++ projeto_rpg.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <time.h>
 #include "manual.h"
 
-int main(void){
-    setlocale(LC_ALL,"portuguese");
-    int r;
-    srand(time(NULL));
-    r = rand() % 7;
-    if(r<1){
-        r++;
-    }
 
-    for(int i=0;i<1;i++){
-       printf("%s, ",tipos[r]);
-       printf("%s, ",Nomes_M[r]);
-       printf("%s \n\n",Nomes_F[r]);
-    }
+int main(void){
+   setlocale(LC_ALL,"portuguese");
+   int r;
+   srand(time(NULL));
+   
+   int rand_tipo; 
+   int rand_nomes_m;
+   int rand_nomes_f;
+
+   rand_tipo = rand() % qtd_tipos;
+   rand_nomes_m = rand() % qtd_nomes_m;
+   rand_nomes_f = rand() % qtd_nomes_f;
+   //r = rand() % 6;
+ 
+   for(int i=0;i<1;i++){
+      printf("%s, ",tipos[rand_tipo]);
+      printf("%s, ",Nomes_M[rand_nomes_m]);
+      printf("%s \n\n",Nomes_F[rand_nomes_f]);
+      
+   }
 }
