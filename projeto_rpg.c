067508ab93f1cb88730d03b73b733f02023f47a6
AHRGO <60015667+AHRGO@users.noreply.github.com>
2022-05-24 10:29:38 -0300

    Voltando ao código anterior
    Testada nova ideia de inclusão na struct. Não ocorreu nenhuma mudança, ainda não funcionando.

--- projeto_rpg.c
+++ projeto_rpg.c
@@ -21,16 +21,16 @@ int main(void){
 
    acolito.nome = "Acólito";
    acolito.personalidade =
-   [
-      ["Eu idolatro um herói particular da minha fé, e constantemente me refiro a seus feitos e exemplos."],
-      ["Eu consigo encontrar semelhanças mesmo entre o inimigos mais violentos, com empatia e sempre trabalhando pela paz."],
-      ["Eu vejo presságios em cada evento e ação. Os deuses estão falando conosco, nós apenas temos de ouvi-los."],
-      ["Nada pode abalar minha atitude otimista."],
-      ["Eu cito (corretamente ou não) textos sagrados e provérbios em quase qualquer situação."],
-      ["Eu sou tolerante (ou intolerante) a qualquer outra fé, e respeito (ou condeno) a adoração a outros deuses."],
-      ["Eu aprecio comida requintada, bebidas e a elite entre o alto escalão de meu templo. Uma vida dura me irrita."],
-      ["Eu passei tanto tempo no templo que possuo pouca prática em lidar com as pessoas mundo a fora."]
-   ];
+   {
+      {"Eu idolatro um herói particular da minha fé, e constantemente me refiro a seus feitos e exemplos."},
+      {"Eu consigo encontrar semelhanças mesmo entre o inimigos mais violentos, com empatia e sempre trabalhando pela paz."},
+      {"Eu vejo presságios em cada evento e ação. Os deuses estão falando conosco, nós apenas temos de ouvi-los."},
+      {"Nada pode abalar minha atitude otimista."},
+      {"Eu cito (corretamente ou não) textos sagrados e provérbios em quase qualquer situação."},
+      {"Eu sou tolerante (ou intolerante) a qualquer outra fé, e respeito (ou condeno) a adoração a outros deuses."},
+      {"Eu aprecio comida requintada, bebidas e a elite entre o alto escalão de meu templo. Uma vida dura me irrita."},
+      {"Eu passei tanto tempo no templo que possuo pouca prática em lidar com as pessoas mundo a fora."}
+   };
    acolito.ideal = 
    {
       {"As tradições ancestrais de adoração e sacrifício devem ser preservadas e perpetradas. (Leal)"},
