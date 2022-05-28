ddcc7d998c9f9583914036c0dfd8b0fbf3fb3a07
Victor Hugo <victor.030farias@gmail.com>
2022-05-28 09:34:49 -0300

    Corrigindo UTF8

--- Antecedente.h
+++ Antecedente.h
@@ -18,49 +18,49 @@ struct Antecedentes {
 
 void antecedentes_lista(){
 
-    acolito.nome = "Ac�lito";
+    acolito.nome = "Acólito";
     strcpy(acolito.personalidade[0],"Eu idolatro um herói particular da minha fé, e constantemente me refiro a seus feitos e exemplos.");
-    strcpy(acolito.personalidade[1],"Eu consigo encontrar semelhan�as mesmo entre o inimigos mais violentos, com empatia e sempre trabalhando pela paz.");
-    strcpy(acolito.personalidade[2],"Eu vejo press�gios em cada evento e a��o. Os deuses est�o falando conosco, n�s apenas temos de ouvi-los.");
+    strcpy(acolito.personalidade[1],"Eu consigo encontrar semelhanças mesmo entre o inimigos mais violentos, com empatia e sempre trabalhando pela paz.");
+    strcpy(acolito.personalidade[2],"Eu vejo presságios em cada evento e ação. Os deuses estão falando conosco, nós apenas temos de ouvi-los.");
     strcpy(acolito.personalidade[3],"Nada pode abalar minha atitude otimista.");
-    strcpy(acolito.personalidade[4],"Eu cito (corretamente ou n�o) textos sagrados e prov�rbios em quase qualquer situa��o.");
-    strcpy(acolito.personalidade[5],"Eu sou tolerante (ou intolerante) a qualquer outra f�, e respeito (ou condeno) a adora��o a outros deuses.");
+    strcpy(acolito.personalidade[4],"Eu cito (corretamente ou não) textos sagrados e provérbios em quase qualquer situação.");
+    strcpy(acolito.personalidade[5],"Eu sou tolerante (ou intolerante) a qualquer outra fé, e respeito (ou condeno) a adoração a outros deuses.");
     strcpy(acolito.personalidade[6],"Eu aprecio comida requintada, bebidas e a elite entre o alto escal�o de meu templo. Uma vida dura me irrita.");
-    strcpy(acolito.personalidade[7],"Eu passei tanto tempo no templo que possuo pouca pr�tica em lidar com as pessoas mundo a fora.");
+    strcpy(acolito.personalidade[7],"Eu passei tanto tempo no templo que possuo pouca prática em lidar com as pessoas mundo a fora.");
 
-    strcpy(acolito.ideal[0],"As tradi��es ancestrais de adora��o e sacrif�cio devem ser preservadas e perpetradas. (Leal)");
-    strcpy(acolito.ideal[1],"Eu sempre tento ajudar aqueles em necessidade, n�o importando o custo pessoal. (Bom)");
-    strcpy(acolito.ideal[2],"N�s devemos ajudar a conduzir as mudan�as que os deuses est�o constantemente trabalhando para o mundo. (Ca�tico)");
-    strcpy(acolito.ideal[3],"Eu espero que um dia eu consiga chegar ao topo na hierarquia da minha religi�o. (Leal)");
-    strcpy(acolito.ideal[4],"Eu acredito que minha divindade guia minhas a��es. Eu tenho f� que, se eu trabalhar duro, coisas boas acontecer�o. (Leal)");
-    strcpy(acolito.ideal[5],"Aspira��o. Eu busco ser digno da gra�a do meu deus ao corresponder minhas a��es aos seus ensinamentos. (Qualquer)");
+    strcpy(acolito.ideal[0],"As tradições ancestrais de adoraçãoo e sacrifício devem ser preservadas e perpetradas. (Leal)");
+    strcpy(acolito.ideal[1],"Eu sempre tento ajudar aqueles em necessidade, não importando o custo pessoal. (Bom)");
+    strcpy(acolito.ideal[2],"Nós devemos ajudar a conduzir as mudanças que os deuses estão constantemente trabalhando para o mundo. (Caótico)");
+    strcpy(acolito.ideal[3],"Eu espero que um dia eu consiga chegar ao topo na hierarquia da minha religião. (Leal)");
+    strcpy(acolito.ideal[4],"Eu acredito que minha divindade guia minhas ações. Eu tenho fé que, se eu trabalhar duro, coisas boas acontecerão. (Leal)");
+    strcpy(acolito.ideal[5],"Aspiração. Eu busco ser digno da graça do meu deus ao corresponder minhas ações aos seus ensinamentos. (Qualquer)");
 
-    strcpy(acolito.vinculo[0],"Eu morreria para recuperar uma rel�quia ancestral de minha f�, perdida h� muito tempo.");
-    strcpy(acolito.vinculo[1],"Eu ainda terei minha vingan�a contra o templo corrupto que me acusou de heresia.");
+    strcpy(acolito.vinculo[0],"Eu morreria para recuperar uma relíquia ancestral de minha fé, perdida há muito tempo.");
+    strcpy(acolito.vinculo[1],"Eu ainda terei minha vingança contra o templo corrupto que me acusou de heresia.");
     strcpy(acolito.vinculo[2],"Eu devo minha vida ao sacerdote que me acolheu quando meus pais morreram.");
-    strcpy(acolito.vinculo[3],"Tudo o que fa�o, fa�o pelo povo.");
+    strcpy(acolito.vinculo[3],"Tudo o que faço, faço pelo povo.");
     strcpy(acolito.vinculo[4],"Eu farei qualquer coisa para proteger o templo que sirvo.");
-    strcpy(acolito.vinculo[5],"Eu busco guardar um texto sagrado que meus inimigos dizem ser her�tico e tentam destru�-lo.");
+    strcpy(acolito.vinculo[5],"Eu busco guardar um texto sagrado que meus inimigos dizem ser her�tico e tentam destruí-lo.");
 
     strcpy(acolito.defeito[0],"Eu julgo os outros severamente, e a mim mesmo mais ainda.");
-    strcpy(acolito.defeito[1],"Eu deposito muita confian�a naqueles que det�m o poder na hierarquia de meu templo.");
-    strcpy(acolito.defeito[2],"Minha devo��o � muitas vezes me cega perante aqueles que professam a f� do meu deus.");
-    strcpy(acolito.defeito[3],"Meu pensamento � inflex�vel.");
+    strcpy(acolito.defeito[1],"Eu deposito muita confiança naqueles que detém o poder na hierarquia de meu templo.");
+    strcpy(acolito.defeito[2],"Minha devoção é muitas vezes me cega perante aqueles que professam a fé do meu deus.");
+    strcpy(acolito.defeito[3],"Meu pensamento é inflex�vel.");
     strcpy(acolito.defeito[4],"Eu suspeito de estranhos e sempre espero o pior deles.");
-    strcpy(acolito.defeito[5],"Depois de escolher um objetivo, eu fico obcecado em cumpri-lo, at� mesmo em detrimento de qualquer outra coisa em minha vida.");
+    strcpy(acolito.defeito[5],"Depois de escolher um objetivo, eu fico obcecado em cumpri-lo, até mesmo em detrimento de qualquer outra coisa em minha vida.");
 
 
-    artesao.nome = "Artes�o da Guilda";
-    strcpy(artesao.personalidade[0],"Eu acredito que tudo que valha a pena fazer, vale a pena ser feito direito. Eu n�o posso evitar � Eu sou perfeccionista.");
-    strcpy(artesao.personalidade[1],"Eu sou um esnobe que olha de cima a baixo aqueles que n�o sabem apreciar artes requintadas.");
+    artesao.nome = "Artesão da Guilda";
+    strcpy(artesao.personalidade[0],"Eu acredito que tudo que valha a pena fazer, vale a pena ser feito direito. Eu não posso evitar � Eu sou perfeccionista.");
+    strcpy(artesao.personalidade[1],"Eu sou um esnobe que olha de cima a baixo aqueles que não sabem apreciar artes requintadas.");
     strcpy(artesao.personalidade[2],"Eu sempre quero aprender como as coisas funcionam e o que deixa as pessoas motivadas.");
-    strcpy(artesao.personalidade[3],"Eu sou cheio de aforismos espirituosos e tenho um proverbio para cada ocasi�o.");
-    strcpy(artesao.personalidade[4],"Eu sou grosso com as pessoas que n�o tem o mesmo comprometimento que eu com o trabalho duro e honesto.");
-    strcpy(artesao.personalidade[5],"Eu gosto de falar longamente sobre minha profiss�o.");
-    strcpy(artesao.personalidade[6],"Eu n�o gasto meu dinheiro facilmente e vou barganhar incansavelmente para conseguir o melhor acordo poss�vel.");
-    strcpy(artesao.personalidade[7],"Eu sou bem conhecido pelo meu trabalho e quero ter certeza que todos o apreciam. Eu sempre fico surpreso quando conhe�o pessoas que n�o ouviram falar de mim.");
+    strcpy(artesao.personalidade[3],"Eu sou cheio de aforismos espirituosos e tenho um proverbio para cada ocasião.");
+    strcpy(artesao.personalidade[4],"Eu sou grosso com as pessoas que não tem o mesmo comprometimento que eu com o trabalho duro e honesto.");
+    strcpy(artesao.personalidade[5],"Eu gosto de falar longamente sobre minha profissão.");
+    strcpy(artesao.personalidade[6],"Eu não gasto meu dinheiro facilmente e vou barganhar incansavelmente para conseguir o melhor acordo possível.");
+    strcpy(artesao.personalidade[7],"Eu sou bem conhecido pelo meu trabalho e quero ter certeza que todos o apreciam. Eu sempre fico surpreso quando conheço pessoas que não ouviram falar de mim.");
 
-    strcpy(artesao.ideal[0],"� dever de todo cidad�o civilizado fortalecer os elos da comunidade e a seguran�a da civiliza��o. (Leal)");
+    strcpy(artesao.ideal[0],"É dever de todo cidadão civilizado fortalecer os elos da comunidade e a segurança da civiliza��o. (Leal)");
     strcpy(artesao.ideal[1],"Meus talentos me foram dados para que eu pudesse us�-los para beneficiar o mundo. (Bom)");
     strcpy(artesao.ideal[2],"Todos deveriam ser livres para perseguir seus pr�prios meios de vida. (Ca�tico)");
     strcpy(artesao.ideal[3],"Eu s� estou aqui pelo dinheiro. (Mau)");
